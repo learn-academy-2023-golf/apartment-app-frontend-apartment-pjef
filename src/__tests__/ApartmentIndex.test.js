9e1f7ceb78675d26eb03e13a383719cafafe0dd5
Eric McKee <lyolayale19@gmail.com>
2023-12-15 09:13:41 -0800

    Completed apartment index component testing, functionality, and styling.

--- src/__tests__/ApartmentIndex.test.js
+++ src/__tests__/ApartmentIndex.test.js
@@ -1,40 +1,46 @@
-import { render, screen } from '@testing-library/react';
-import ApartmentIndex from '../pages/ApartmentIndex';
-import { BrowserRouter } from 'react-router-dom';
-import mockApartments from '../mockApartments';
+import { render, screen } from "@testing-library/react";
+import ApartmentIndex from "../pages/ApartmentIndex";
+import { BrowserRouter } from "react-router-dom";
+import mockApartments from "../mockApartments";
 
 describe("<ApartmentIndex />", () => {
   beforeEach(() => {
     const currentUser = {
       email: "test@test.com",
-      id: 1
-    }
+      id: 1,
+    };
     render(
       <BrowserRouter>
-        <ApartmentIndex apartments={mockApartments}/>
+        <ApartmentIndex apartments={mockApartments} />
       </BrowserRouter>
-    )
-  })
+    );
+  });
   it("renders without crashing", () => {
-    const element = screen.getByText("View Listings:")
-    expect(element).toBeInTheDocument()
-  })
+    const element = screen.getByText("View Listings:");
+    expect(element).toBeInTheDocument();
+  });
+
+  it("contains buttons", () => {
+    const allButtons = document.querySelectorAll("button");
+    allButtons.forEach((button) => {
+      expect(button).toBeInTheDocument();
+    });
+  });
 
   it("renders apartment cards", () => {
-    mockApartments.forEach(apartment => {
-        const price = screen.getByText(`$${apartment.price}/month`)
-        expect(price).toBeInTheDocument()
-        const street = screen.getByText(apartment.street)
-        expect(street).toBeInTheDocument()
-        const city = screen.getByText(apartment.city)
-        expect(city).toBeInTheDocument()
-        const state = screen.getByText(apartment.state)
-        expect(state).toBeInTheDocument()
-        const bedrooms = screen.getByText(apartment.bedrooms)
-        expect(bedrooms).tobeInTheDocument()
-        const bathrooms = screen.getByText(apartment.bathrooms)
-        expect(bathrooms).toBeInTheDocument()
-        
-    })
-  })
-})
+    mockApartments.forEach((apartment) => {
+      const price = screen.getByText(`$${apartment.price}/month`);
+      expect(price).toBeInTheDocument();
+
+      const address = screen.getByText(
+        `${apartment.street}, ${apartment.city}, ${apartment.state}`
+      );
+      expect(address).toBeInTheDocument();
+
+      const rooms = screen.getAllByText(
+        `${apartment.bedrooms} Bedroom ${apartment.bathrooms} Bath`
+      );
+      rooms.forEach((room) => expect(room).toBeInTheDocument());
+    });
+  });
+});
